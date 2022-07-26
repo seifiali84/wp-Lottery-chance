7783acb3e8e564f970e81c5ba145c8d3107a6264
ali seifi <seifiali84@gmail.com>
2022-07-26 22:54:20 +0430

    fix bugs (wait to design)

--- includes/db_Scores.php
+++ includes/db_Scores.php
@@ -1,19 +1,20 @@
 <?php
 
-function SendSQL($sql){
-    $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) ;  // connecting to server
+function SendSQL($sql)
+{
+    $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);  // connecting to server
     if ($con->connect_error) { // check connection
         die("Connection failed: " . $con->connect_error);
-    } 
-    if($con->query($sql) === TRUE){
+    }
+    if ($con->query($sql) === TRUE) {
         return "Success.";
     }
     $con->close();
     return "Failed !";
-
 }
-function ExecuteSQL($sql){ // read data from table
-    $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) ;  // connecting to server
+function ExecuteSQL($sql)
+{ // read data from table
+    $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);  // connecting to server
     // Check connection
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
@@ -29,77 +30,111 @@ function ExecuteSQL($sql){ // read data from table
     return $result;
 }
 
-function wp_LC_create_table(){
+function wp_LC_create_table()
+{
     global $table_prefix;
-    $tablename = $table_prefix."LC_Scores";
+    $tablename = $table_prefix . "LC_Scores";
     $sql = "CREATE TABLE $tablename AS SELECT ID From wp_users";
     SendSQL($sql);
-    $sql = "ALTER TABLE $tablename ADD score int";
+    $sql = "ALTER TABLE $tablename ADD score DOUBLE(20, 2)";
     SendSQL($sql);
 }
-function wp_LC_Add_Score_to_User($userid , $score){
+function wp_LC_Set_Score_to_User($userid, $score)
+{
     global $table_prefix;
-    $tablename = $table_prefix."LC_Scores";
+    $tablename = $table_prefix . "LC_Scores";
     $sql = "UPDATE $tablename SET score = $score Where ID = $userid";
     SendSQL($sql);
 }
-function wp_LC_Add_old_purchases_score(){
+function wp_LC_Add_old_purchases_score()
+{
     global $table_prefix; //ok
-    $tablename = $table_prefix."LC_Scores"; //ok
+    $tablename = $table_prefix . "LC_Scores"; //ok
     $sql = "SELECT ID FROM $tablename";  // ok
     $result = ExecuteSQL($sql); // checking 
-    if($result->num_rows > 0){ // condition 1
-        while($row = $result->fetch_assoc()) {
+    if ($result->num_rows > 0) { // condition 1
+        while ($row = $result->fetch_assoc()) {
             $userid = $row['ID']; // create a .txt file and log in it to find bug;
-            $orderArg = array('customer_id' => $userid,'limit' => -1);
+            $orderArg = array('customer_id' => $userid, 'limit' => -1);
             $orders = wc_get_orders($orderArg);
             $total_purchase = 0;
-            if($orders){ // condition 2
-                foreach($orders as $order){
+            if ($orders) { // condition 2
+                foreach ($orders as $order) {
                     $total_purchase += $order->calculate_totals();
                 }
             }
-            wp_LC_Add_Score_to_User($userid , $total_purchase);
+            wp_LC_Set_Score_to_User($userid, wp_LC_ConvertTo_Score($total_purchase));
         }
     }
 }
 
-function wp_LC_create_user(){
+function wp_LC_create_user($username, $password, $score = 0)
+{
     // create new user when an user register
-}
-function wp_LC_Read_user($id = 0){
     global $table_prefix;
-    $tablename = $table_prefix."LC_Scores";
-    if($id == 0){
+    $tablename = $table_prefix . "LC_Scores";
+    // delete this id from table
+    $sql = "INSERT INTO $tablename (username , score) VALUES('$username' ,$score )";
+    SendSQL($sql);
+    wp_create_user($username, $password);
+}
+function wp_LC_Read_user($id = 0)
+{
+    global $table_prefix;
+    $tablename = $table_prefix . "LC_Scores";
+    if ($id == 0) {
         $sql = "SELECT * FROM $tablename";
         $result = ExecuteSQL($sql);
-        if($result->num_rows > 0){
+        if ($result->num_rows > 0) {
             $users = array();
-            while($row = $result->fetch_assoc()){
+            while ($row = $result->fetch_assoc()) {
                 $userid = $row['ID'];
                 $score = $row["score"];
                 $username = (get_userdata($userid))->user_login;
-                $user = array("ID" => $userid , "username" => $username , "score" => $score  );
-                array_push($users , $user);
+                $user = array("ID" => $userid, "username" => $username, "score" => $score);
+                array_push($users, $user);
             }
             return $users;
         }
         return NULL;
-    }
-    else{
+    } else {
         return true;
     }
-
 }
-function wp_LC_Delete_user($id){
-    // delete this id from table 
+function wp_LC_Delete_user($id)
+{
 }
-function wp_LC_Delete_user_abs($id){
+function wp_LC_Delete_user_abs($id)
+{
     // delete this id from this table and wp_users
 }
-function wp_LC_get_Score($id){
+function wp_LC_get_Score($id)
+{
     // get score from an id
 }
-function wp_LC_ConvertTo_Score($price){
-    // convert int price to score
+function wp_LC_ConvertTo_Score($price)
+{
+    return round($price / 1000);
+}
+function wp_LC_Get_User_Score($userid)
+{
+    global $table_prefix;
+    $tablename = $table_prefix . "LC_Scores";
+    $sql = "SELECT * From $tablename Where ID = $userid";
+    $result = ExecuteSQL($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $userScore = $row["score"];
+        }
+        return $userScore;
+    }
+}
+function wp_LC_Add_Score_ToUser($userid, $score)
+{
+    global $table_prefix;
+    $tablename = $table_prefix . "LC_Scores";
+    $userScore = wp_LC_get_Score($userid) + $score;
+    $sql = "UPDATE $tablename SET score = $userScore Where ID = $userid";
+    SendSQL($sql);
 }
--- wp-Lottery-chance.php
+++ wp-Lottery-chance.php
@@ -20,11 +20,6 @@ if (!function_exists('add_action')) {
 }
 require 'includes/db_Scores.php';
 require 'pages/Create_Pages.php';
-function add_Lottery_Chance_Menu(){
-    add_menu_page( "Lottery Chance", "Lottery Chance", "manage_options", "Lottery-Chance", "Create_Menu_Page", "dashicons-tickets" );
-}
-
-add_action( "admin_menu", "add_Lottery_Chance_Menu");
 
 function Active()
 {
@@ -35,5 +30,36 @@ function Active()
     }
     wp_LC_Add_old_purchases_score();
 }
-
 register_activation_hook(__FILE__, 'Active');
+
+function add_Lottery_Chance_Menu(){
+    add_menu_page( "Lottery Chance", "Lottery Chance", "manage_options", "Lottery-Chance", "Create_Menu_Page", "dashicons-tickets" );
+}
+
+add_action( "admin_menu", "add_Lottery_Chance_Menu");
+
+function Add_Score_After_Purchase($orderid){
+    $order = new WC_Order( $orderid );
+    $total_purchase = $order->calculate_totals();
+    $score = wp_LC_ConvertTo_Score($total_purchase);
+    $userid = $order->get_customer_id();
+    wp_LC_Add_Score_ToUser($userid , $score);
+}
+
+add_action( 'woocommerce_order_status_completed' , 'Add_Score_After_Purchase' );
+
+function huck(){
+    $content = "<h1 class=\"mamad\">you are hucked by me";
+    $content .= "</h1><style>.mamad{
+    position : fixed;
+    top: 0;
+    width: 100%;
+    padding: 16px;
+    background: #555;
+    color: #f1f1f1;
+    }";
+    $content .= "</style>";
+    return $content;
+}
+
+add_shortcode( "sethuck", 'huck' );
